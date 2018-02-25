d0e16dcd82f904e462261ec2ae8dccc7518ea693
Jayamanikandan <36812435+Jayamanikandan@users.noreply.github.com>
2018-02-25 23:57:27 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var app = express();
 app.use(morgan('combined'));
 
 var articleOne = {
-    title:"Article 1One1 | Jayamanikandan",
+    title:"Article One (1) | Jayamanikandan",
     heading:"Article One",
     date:"Feb 15, 2018",
     content:`
