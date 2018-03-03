3aa197cf40f96680b5683dd5c4445f2de17d5510
Jayamanikandan <36812435+Jayamanikandan@users.noreply.github.com>
2018-03-03 15:21:07 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,20 +121,19 @@ app.get('/:articleName', function(req,res){
 );*/
 
 app.get('/articles/:articleName', function(req,res){
-    pool.query("SELECT * FROM Article WHERE title='" + req.params.articleName + "'", function(err,result)
-        {
+    pool.query("SELECT * FROM Article WHERE title ='" + req.params.articleName + "'", function(err,result){
             if(err)
             {
                 res.status(500).send(err.toString());
             }
             else
             {
-                if(res.rows.length === 0)
+                if(result.rows.length === 0)
                 {
-                    res.status(404).send("Article NOT found");
+                    res.status(404).send('Article NOT found');
                 }
                 else{
-                    var articleData = res.rows[0];
+                    var articleData = result.rows[0];
                     res.send(createTemplate(articleData));
                 }
             }
