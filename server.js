c8fe13816ca92f94a31be2eb563273cb4cfb9f2e
Jayamanikandan <36812435+Jayamanikandan@users.noreply.github.com>
2018-03-03 14:03:12 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -121,7 +121,7 @@ app.get('/:articleName', function(req,res){
 );*/
 
 app.get('/articles/:articleName', function(req,res){
-    pool.query("SELECT * FROM article WHERE title='" + req.params.articleName + "';", function(err,result)
+    pool.query("SELECT * FROM article WHERE title='" + req.params.articleName + "'", function(err,result)
         {
             if(err)
             {
